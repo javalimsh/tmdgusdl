81019812b3b7da157aa0af69491f5e8f8979a3e9
PC <PC@218.50.113.85>
2019-10-16 00:06:27 +0900

    메세지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
+
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello world");
-	System.out.println("Hi world");
+	System.out.println("Hello1 world");
+	System.out.println("Hello2 world");
+	
+
 }
 }
